8adc790a2f51a8876de3aa2f05be31b27fcddb6d
Greg DeKoenigsberg <greg@eucalyptus.com>
2015-12-03 17:02:51 -0500

    Stub for issuebot

---
+++ issuebot.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#
+# Here's the future issuebot!
+# 
+# REQUIREMENTS FOR 1.0:
+#
+# MUCH SIMPLIFIED:
+# 
+# * Template to point out the file, so we can find the maintainer
+# * Detect filename, so we can ping the maintainer
+# * Look to see who last responded (submitter or maintainer) so we can ping the other side
+# * Figure out how to close really old stuff (maybe every 3 months ask submitter "still a bug?")
