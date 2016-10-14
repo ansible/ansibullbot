f11509621f0eba9eb69979ada7640462b36f60b5
Greg DeKoenigsberg <greg.dekoenigsberg@gmail.com>
2016-10-14 12:37:55 -0400

    Adding rmorlok as the maintainer of all ecs modules in extras

--- modules-over-time.sh
+++ modules-over-time.sh
@@ -7,16 +7,17 @@ repohome='/Users/gregdek/Desktop/ANSIBLE/REPOS/ansible'
 # to wherever your git checkouts are.
 
 # Get the latest bits
-cd $repohome/ansible-modules-core
+# cd $repohome/ansible-modules-core
+cd $repohome/ansible-modules-extras
 git pull
 
 # This ugly one-liner gets the first commit date of every .py file
 # in the repo, excluding __init__ files and deprecated files.
 
-# find . -name "*.py" | grep -v '__init__' | grep -v '/_' | xargs -I {} sh -c "echo {}; git log --format='%ai' {} | sort | head -n 1" >> /tmp/modules-dates.txt
+find . -name "*.py" | grep -v '__init__' | grep -v '/_' | xargs -I {} sh -c "echo {}; git log --format='%ai' {} | sort | head -n 1" > /tmp/modules-dates.txt
 
 # Now let's take that file and chop it up a bit.
-# perl -pi.bak1 -e 's/py\n/py /g;' /tmp/modules-dates.txt
+perl -pi.bak1 -e 's/py\n/py /g;' /tmp/modules-dates.txt
 
 # Now let's spit out the final list.
 perl -ne 'print "$2 $1\n" if m/([^\s]*) ([^\s]*)/;' /tmp/modules-dates.txt | sort
