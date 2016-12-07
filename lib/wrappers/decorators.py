4dc58ac91f6364888544f57a15e20b688c386157
James Tanner <tanner.jc@gmail.com>
2016-12-07 11:32:48 -0500

    Change the sleep times accordingly

--- lib/wrappers/decorators.py
+++ lib/wrappers/decorators.py
@@ -2,11 +2,12 @@
 
 # https://github.com/octokit/octokit.net/issues/638#issuecomment-67795998
 
-import functools
-from functools import wraps
-from inspect import getargspec
 import time
 import logging
+import functools
+
+from functools import wraps
+from inspect import getargspec
 
 def RateLimited(fn):
     argspec = getargspec(fn)
@@ -18,7 +19,7 @@ def RateLimited(fn):
         while not success:
             count += 1
             logging.info('rl\'ed call #%s' % count)
-            sminutes = 60
+            sminutes = 5
             try:
                 x = fn(*args, **kwargs)
                 success = True
@@ -28,6 +29,8 @@ def RateLimited(fn):
                 if hasattr(e, 'data') and e.data.get('message'):
                     if 'blocked from content creation' in e.data['message']:
                         sminutes = 2
+                    elif 'rate limit exceeded' in e.data['message']:
+                        sminutes = 61
                     else:
                         import epdb; epdb.st()
                 else:
