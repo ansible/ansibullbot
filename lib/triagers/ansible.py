1f6a112e8432e4123719a9dcada65e60d0683cf5
James Tanner <tanner.jc@gmail.com>
2017-01-17 10:16:35 -0500

    Enable the only_issues arg

--- lib/triagers/ansible.py
+++ lib/triagers/ansible.py
@@ -362,6 +362,10 @@ class AnsibleTriage(DefaultTriager):
                     logging.info(str(number) + ' is issue, skipping')
                     redo = False
                     continue
+                if self.args.only_issues and 'pull' in issue.html_url:
+                    logging.info(str(number) + ' is pullrequest, skipping')
+                    redo = False
+                    continue
 
                 # users may want to re-run this issue after manual intervention
                 redo = True
