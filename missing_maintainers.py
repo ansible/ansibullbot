533c1c422aaaf180c1d468efde6105dbb2fb499c
James Tanner <tanner.jc@gmail.com>
2017-01-18 14:33:40 -0500

    Add the missing_maintainers script and refactor triage classes to be usable outside triage functions

---
+++ missing_maintainers.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+# missing_maintainers.py - List community supported modules without maintainers.
+#
+#   * Uses the ModuleIndexer to get the list of modules and their meta
+#
+
+from lib.triagers.ansible import AnsibleTriage
+
+AT = AnsibleTriage(args={})
+
+mkeys = sorted(AT.module_indexer.modules.keys())
+for mkey in mkeys:
+    v = AT.module_indexer.modules[mkey]
+    if v.get('deprecated') is True:
+        continue
+    if v['metadata']:
+        if 'supported_by' not in v['metadata']:
+            #import epdb; epdb.st()
+            continue
+        if v['metadata']['supported_by'] != 'community':
+            continue
+        if not v['maintainers'] or v['maintainers'] == ['ansible']:
+            print('%s,%s,%s' %
+                  (mkey, v['metadata']['supported_by'], v['maintainers']))
