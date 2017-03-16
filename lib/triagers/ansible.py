88bcc0c3251f3caf5912704a08005d25499b5412
Pierre-Louis Bonicoli <pierre-louis.bonicoli@libregerbil.fr>
2017-03-16 16:26:58 +0100

    Ignore issue not found
    issue could be None when using --pr parameter

--- lib/triagers/ansible.py
+++ lib/triagers/ansible.py
@@ -1326,7 +1326,8 @@ class AnsibleTriage(DefaultTriager):
                 for x in numbers:
                     logging.info('fetch %s' % x)
                     issue = self.repos[repo]['repo'].get_issue(x)
-                    issues.append(issue)
+                    if issue:
+                        issues.append(issue)
 
                 self.repos[repo]['issues'] = issues
 
