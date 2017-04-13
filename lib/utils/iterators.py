6e3ea105e8369d3e2ada219a1800c2bd214a3589
James Tanner <tanner.jc@gmail.com>
2017-04-13 12:56:58 -0400

    Reverse the order

--- lib/utils/iterators.py
+++ lib/utils/iterators.py
@@ -6,6 +6,7 @@ class RepoIssuesIterator(object):
     def __init__(self, repo, numbers, issuecache={}):
         self.repo = repo
         self.numbers = sorted(set([int(x) for x in numbers]))
+        self.numbers = reversed(self.numbers)
         self.issuecache = issuecache
         self.i = 0
 
