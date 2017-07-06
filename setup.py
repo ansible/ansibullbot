02671411f68edbc8f8605ed9575f0cfc45ecbd91
James Tanner <tanner.jc@gmail.com>
2017-07-05 21:52:55 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='ansibullbot',
+    version='0.1.0',
+    description='github triage bot',
+    long_description=readme,
+    author='James Tanner',
+    author_email='tanner.jc@gmail.com',
+    url='https://github.com/ansible/ansibullbot',
+    license=license,
+    packages=find_packages(exclude=('test'))
+)
