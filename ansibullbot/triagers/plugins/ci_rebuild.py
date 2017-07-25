f7c8bfb5d72a95e62de754e2edf9a6e49591b04a
James Tanner <tanner.jc@gmail.com>
2017-07-25 07:14:08 -0400

    runids are not always integers

--- ansibullbot/triagers/plugins/ci_rebuild.py
+++ ansibullbot/triagers/plugins/ci_rebuild.py
@@ -1,17 +1,5 @@
 #!/usr/bin/env python
 
-'''
-if self.meta['shipit_actors'] and self.meta['ci_stale']:
-    ci_states = [x for x in iw.pullrequest_status if x['target_url'].startswith('https://app.shippable.com/')]
-    ci_states = sorted(ci_states, key=itemgetter('created_at'))
-
-    if ci_states and ci_states[-1]['state'] == 'success':
-        target_url =  ci_states[-1]['target_url']
-        match = re.match('https://app.shippable.com/github/ansible/ansible/runs/([\d]+)/summary', target_url)
-        if match:
-            # Trigger a new Shippable run if the PR is stalled, last run was a success and someone approved the PR
-            self.SR.rebuild(match.group(1))
-'''
 
 def status_to_date_and_runid(status):
     """convert pr status to a tuple of date and runid"""
@@ -22,7 +10,6 @@ def status_to_date_and_runid(status):
         target = target.split('/')[-2]
     else:
         target = target.split('/')[-1]
-    target = int(target)
     return (created_at, target)
 
 
