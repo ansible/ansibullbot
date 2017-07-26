a5b634579cb86c59f656b944ce0d123bf67e50e4
James Tanner <tanner.jc@gmail.com>
2017-07-25 21:25:46 -0400

    Add more checking+handling for label waffling

--- ansibullbot/errors/__init__.py
+++ ansibullbot/errors/__init__.py
@@ -4,3 +4,7 @@
 # https://stackoverflow.com/a/1319675
 class RateLimitError(Exception):
     """Used to trigger the ratelimiting decorator"""
+
+
+class LabelWafflingError(Exception):
+    """Label has been added/removed too many times"""
