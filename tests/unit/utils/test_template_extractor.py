7ce64b7eb670de73af5632c97de0b0c8565869a9
James Tanner <tanner.jc@gmail.com>
2017-08-08 17:03:06 -0400

    Add more extractor tests

--- tests/unit/utils/test_template_extractor.py
+++ tests/unit/utils/test_template_extractor.py
@@ -31,3 +31,56 @@ class TestTemplateExtraction(unittest.TestCase):
         assert tdata.get('one') == 'section one'
         assert tdata.get('two') == 'section two'
         assert tdata.get('three') == 'section three'
+
+    def test_1(self):
+        body = [
+            '#### ISSUE TYPE',
+            '- Bug Report',
+            '#### COMPONENT NAME',
+            'widget module',
+            '#### ANSIBLE VERSION',
+            '1.9.x'
+            '#### SUMMARY',
+            'the widget module does not work for me!!!'
+        ]
+        body = '\r\n'.join(body)
+        issue_number = 0
+        issue_class = 'issue'
+        sections = ['ISSUE TYPE', 'COMPONENT NAME', 'ANSIBLE VERSION', 'SUMMARY']
+        tdata = extract_template_data(
+            body, issue_number=issue_number,
+            issue_class=issue_class, SECTIONS=sections
+        )
+        assert tdata.get('ansible version') == '1.9.x'
+        assert tdata.get('issue type') == 'bug report'
+        assert tdata.get('component name') == 'widget'
+        assert tdata.get('component_raw') == 'widget module'
+        assert tdata.get('summary') == 'the widget module does not work for me!!!'
+
+    # FIXME
+    '''
+    def test_2(self):
+        body = [
+            '*** issue type ***:',
+            '- Bug Report',
+            '*** componet name ***:',
+            'widget module',
+            '*** ansible version ***:',
+            '1.9.x'
+            '*** summary ***:',
+            'the widget module does not work for me!!!'
+        ]
+        body = '\r\n'.join(body)
+        issue_number = 0
+        issue_class = 'issue'
+        sections = ['ISSUE TYPE', 'COMPONENT NAME', 'ANSIBLE VERSION', 'SUMMARY']
+        tdata = extract_template_data(
+            body, issue_number=issue_number,
+            issue_class=issue_class, SECTIONS=sections
+        )
+        assert tdata.get('ansible version') == '1.9.x'
+        assert tdata.get('issue type') == 'bug report'
+        assert tdata.get('component name') == 'widget'
+        assert tdata.get('component_raw') == 'widget module'
+        assert tdata.get('summary') == 'the widget module does not work for me!!!'
+    '''
