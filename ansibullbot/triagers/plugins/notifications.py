b284f7e33cb7c3526821a4c14bf20994c16687bc
James Tanner <tanner.jc@gmail.com>
2017-09-05 11:27:01 -0400

    cleanup the notification facts

--- ansibullbot/triagers/plugins/notifications.py
+++ ansibullbot/triagers/plugins/notifications.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+
+import logging
+
+
 def get_notification_facts(issuewrapper, meta, file_indexer):
     '''Build facts about mentions/pings'''
     iw = issuewrapper
@@ -13,26 +18,70 @@ def get_notification_facts(issuewrapper, meta, file_indexer):
     # who can be assigned?
     valid_assignees = [x.login for x in iw.repo.assignees]
 
-    # add people from filemap matches
-    if iw.is_pullrequest() or meta.get('guessed_components'):
+    # add people from files and from matches
+    if iw.is_pullrequest() or meta.get('guessed_components') or meta.get('component_matches') or meta.get('module_match'):
 
-        if iw.is_pullrequest():
+        fnotify = []
+        fassign = []
+
+        # this needs to be refactored to handle component matches primarily
+        if meta.get('component_matches'):
+            for cm in meta['component_matches']:
+                fnotify += cm.get('owners', [])
+                fnotify += cm.get('notify', [])
+
+        # add module maintainers
+        if meta.get('module_match'):
+            maintainers = meta.get('module_match', {}).get('maintainers', [])
+
+            # do nothing if not maintained
+            if maintainers:
+
+                # don't ping us...
+                if 'ansible' in maintainers:
+                    maintainers.remove('ansible')
+
+                for maintainer in maintainers:
+
+                    # don't notify maintainers of their own issues ... duh
+                    if maintainer == iw.submitter:
+                        continue
+
+                    if maintainer in valid_assignees and \
+                            maintainer not in current_assignees:
+                        fassign.append(maintainer)
+
+                    if maintainer in nfacts['to_notify']:
+                        continue
+
+                    fnotify.append(maintainer)
+
+        # if component_matches exist, the files were already inspected
+        if iw.is_pullrequest() and not meta.get('component_matches'):
             (fnotify, fassign) = \
                 file_indexer.get_filemap_users_for_files(iw.files)
-        else:
-            (fnotify, fassign) = \
+        # FIXME: not sure where "guessed" came from
+        elif meta.get('guessed_components'):
+            (_fnotify, _fassign) = \
                 file_indexer.get_filemap_users_for_files(meta['guessed_components'])
+            fnotify += _fnotify
+            fassign += _fassign
+
+        fassign = sorted(set(fassign))
+        fnotify = sorted(set(fnotify))
 
         for user in fnotify:
             if user == iw.submitter:
                 continue
-            if user not in nfacts['to_notify']:
-                if not iw.history.last_notified(user) and \
-                        not iw.history.was_assigned(user) and \
-                        not iw.history.was_subscribed(user) and \
-                        not iw.history.last_comment(user):
+            if not iw.history.last_notified(user) and \
+                    not iw.history.was_assigned(user) and \
+                    not iw.history.was_subscribed(user) and \
+                    not iw.history.last_comment(user):
 
-                    nfacts['to_notify'].append(user)
+                nfacts['to_notify'].append(user)
+
+            else:
+                logging.info('{} already notified'.format(user))
 
         for user in fassign:
             if user == iw.submitter:
@@ -42,35 +91,8 @@ def get_notification_facts(issuewrapper, meta, file_indexer):
             if user not in current_assignees and user in valid_assignees:
                 nfacts['to_assign'].append(user)
 
-    # add module maintainers
-    if meta.get('module_match'):
-        maintainers = meta.get('module_match', {}).get('maintainers', [])
+    # prevent duplication
+    nfacts['to_assign'] = sorted(set(nfacts['to_assign']))
+    nfacts['to_notify'] = sorted(set(nfacts['to_notify']))
 
-        # do nothing if not maintained
-        if maintainers:
-
-            # don't ping us...
-            if 'ansible' in maintainers:
-                maintainers.remove('ansible')
-
-            for maintainer in maintainers:
-
-                # don't notify maintainers of their own issues ... duh
-                if maintainer == iw.submitter:
-                    continue
-
-                if maintainer in valid_assignees and \
-                        maintainer not in current_assignees:
-                    nfacts['to_assign'].append(maintainer)
-
-                if maintainer in nfacts['to_notify']:
-                    continue
-
-                if not iw.history.last_notified(maintainer) and \
-                        not iw.history.was_assigned(maintainer) and \
-                        not iw.history.was_subscribed(maintainer) and \
-                        not iw.history.last_comment(maintainer):
-                    nfacts['to_notify'].append(maintainer)
-
-    #import epdb; epdb.st()
     return nfacts
