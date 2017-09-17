bd55e584357c9c386fd79d9f7da765ac82a42fb6
Pierre-Louis Bonicoli <pierre-louis.bonicoli@libregerbil.fr>
2017-09-17 02:41:19 +0200

    Update unit tests

--- tests/unit/utils/test_module_indexer.py
+++ tests/unit/utils/test_module_indexer.py
@@ -12,21 +12,36 @@ from six.moves import mock
 from ansibullbot.utils.moduletools import ModuleIndexer
 
 
-def run(BOTMETA, authors, filepath):
+def run(BOTMETA, filepaths):
+    """Create and test ModuleIndexer
+
+    1. parse BOTMETA metadata
+    2. list source code paths (filepaths keys)
+    3. fetch authors from source code content (filepaths values)
+
+    filepaths = {
+        'lib/ansible/modules/foo/bar/baz.py': None, # no author in source code
+        'lib/ansible/modules/foo2/bar2/baz.py': ['author1', 'author2'],
+    }
+    """
 
     def set_modules(self):
         '''list modules from filesystem'''
-        m = copy.deepcopy(ModuleIndexer.EMPTY_MODULE)
-        m['filename'] = os.path.basename(filepath)
-        m['filepath'] = filepath
-        self.modules[m['filepath']] = m
+        for filepath in filepaths.keys():
+            m = copy.deepcopy(ModuleIndexer.EMPTY_MODULE)
+            m['filename'] = os.path.basename(filepath)
+            m['filepath'] = filepath
+            self.modules[filepath] = m
 
     def set_authors(self, mfile):
         '''set authors from module source code: 'author' field in DOCUMENTATION metadata'''
-        if mfile.endswith(filepath):
-            return authors
-        else:
-            assert False
+        for (filepath, authors) in filepaths.items():
+            if mfile.endswith(filepath):
+                if not authors:
+                    return []
+                else:
+                    return authors
+        assert False
 
     @mock.patch.object(ModuleIndexer, 'update')
     @mock.patch.object(ModuleIndexer, 'get_module_authors', side_effect=set_authors, autospec=True)
@@ -42,6 +57,32 @@ def run(BOTMETA, authors, filepath):
 
 
 class TestModuleIndexer(TestCase):
+    BOTMETA = """
+    ---
+    macros:
+        team_green:
+          - larry
+          - cow
+        modules: lib/ansible/modules
+    files:
+        # Simplest test
+        $modules/foo/bar: jim
+        $modules/foo/bar/baz.py: bob
+
+        # Check path normalization
+        $modules/foo2/bar2/: jim
+        $modules/foo2/bar2/baz.py: bob
+
+        # Check that only full paths match
+        $modules/bar/foo: csim
+        $modules/bar/foofoo.py: uolip
+
+        # Check with two parent directories
+        $modules/baz:
+            maintainers: [ZaZa, Loulou]
+        $modules/baz/test: jim
+        $modules/baz/test/code.py: bob
+    """
 
     def test_maintainers(self):
         """maintainers don't get mix up
@@ -63,15 +104,29 @@ class TestModuleIndexer(TestCase):
             packaging/:
               maintainers: csim
         """
+        expected = {
+            'packaging': {
+                'maintainers': ['csim'],
+                'maintainers_keys': ['packaging'],
+                'labels': ['packaging'],
+            }
+        }
 
         filepath = 'lib/ansible/modules/packaging/apt.py'
-        indexer = run(textwrap.dedent(BOTMETA), ['Hubert'], filepath)
+        filepaths = {filepath: ['Hubert']}
+        indexer = run(textwrap.dedent(BOTMETA), filepaths)
+
+        self.assertEqual(indexer.botmeta['files'], expected)
+
         self.assertEqual(len(indexer.modules), 1)  # ensure only fake data are loaded
         self.assertEqual(indexer.modules[filepath]['maintainers'], ['Hubert'])
-        self.assertEqual(indexer.modules[filepath]['maintainers_key'], None)
+        self.assertFalse(indexer.modules[filepath]['maintainers_keys'])
 
     def test_module_authors_inherit_from_directory_maintainers(self):
-        """ authors defined in module metadata inherit from directory maintainers specified in BOTMETA
+        """Check maintainers
+
+        Authors defined in 'author' field of 'DOCUMENTATION' module metadata inherit from
+        directory maintainers specified in BOTMETA.
 
         - author is defined in 'author' field of 'DOCUMENTATION' module metadata ('bob')
         - module path not in BOTMETA ('lib/ansible/modules/foo/bar/baz.py')
@@ -88,33 +143,99 @@ class TestModuleIndexer(TestCase):
             $modules/foo/bar: jim
         """
 
+        expected = {
+            'lib/ansible/modules/foo/bar': {
+                'labels': sorted(['lib', 'ansible', 'modules', 'foo', 'bar']),
+                'maintainers': ['jim'],
+                'maintainers_keys': ['lib/ansible/modules/foo/bar'],
+            }
+        }
+
         filepath = 'lib/ansible/modules/foo/bar/baz.py'
-        indexer = run(textwrap.dedent(BOTMETA), ['bob'], filepath)
+        filepaths = {filepath: ['bob']}
+        indexer = run(textwrap.dedent(BOTMETA), filepaths)
+
+        self.assertEqual(indexer.botmeta['files'], expected)
+
         self.assertEqual(len(indexer.modules), 1)  # ensure only fake data are loaded
         self.assertEqual(set(indexer.modules[filepath]['maintainers']), set(['bob', 'jim']))  # both
-        self.assertEqual(indexer.modules[filepath]['maintainers_key'], 'lib/ansible/modules/foo/bar')
+        self.assertEqual(indexer.modules[filepath]['maintainers_keys'], ['lib/ansible/modules/foo/bar'])
 
-    def test_maintainers_dont_inherit_from_directory_maintainers(self):
-        """module maintainers don't inherit from parent directory maintainers (both defined in BOTMETA)
+    def test_maintainers_inherit_from_directory_maintainers(self):
+        """Check maintainers inheritance
 
-        - author isn't defined in 'author' field of 'DOCUMENTATION' module metadata
-        - module path in BOTMETA ('lib/ansible/modules/foo/bar/baz.py')
-        - one parent directory in BOTMETA with one maintainer ('lib/ansible/modules/foo/bar')
-        """
-        BOTMETA = """
-        ---
-        macros:
-            team_green:
-              - larry
-              - cow
-            modules: lib/ansible/modules
-        files:
-            $modules/foo/bar: jim
-            $modules/foo/bar/baz.py: bob
+        No author defined in 'author' field of 'DOCUMENTATION' module metadata.
         """
 
-        filepath = 'lib/ansible/modules/foo/bar/baz.py'
-        indexer = run(textwrap.dedent(BOTMETA), [], filepath)
-        self.assertEqual(len(indexer.modules), 1)  # ensure only fake data are loaded
-        self.assertEqual(set(indexer.modules[filepath]['maintainers']), set(['bob']))  # only bob, not jim
-        self.assertEqual(indexer.modules[filepath]['maintainers_key'], 'lib/ansible/modules/foo/bar/baz.py')
+        filepaths = {
+            'lib/ansible/modules/foo/bar/baz.py': None,
+            'lib/ansible/modules/foo2/bar2/baz.py': None,
+            'lib/ansible/modules/bar/foofoo.py': None,
+            'lib/ansible/modules/baz/test/code.py': None,
+        }
+
+        expected = {}
+        key = 'lib/ansible/modules/foo/bar'
+        expected[key] = {
+            'maintainers': ['jim'],
+            'maintainers_keys': [key],
+        }
+        key = 'lib/ansible/modules/foo/bar/baz.py'
+        expected[key] = {
+            'maintainers': ['bob', 'jim'],
+            'maintainers_keys': ['lib/ansible/modules/foo/bar', key],
+        }
+        key = 'lib/ansible/modules/foo2/bar2'
+        expected[key] = {
+            'maintainers': ['jim'],
+            'maintainers_keys': [key],
+        }
+        key = 'lib/ansible/modules/foo2/bar2/baz.py'
+        expected[key] = {
+            'maintainers': ['bob', 'jim'],
+            'maintainers_keys': ['lib/ansible/modules/foo2/bar2', key],
+        }
+        key = 'lib/ansible/modules/bar/foo'
+        expected[key] = {
+            'maintainers': ['csim'],
+            'maintainers_keys': [key],
+        }
+
+        key = 'lib/ansible/modules/bar/foofoo.py'
+        expected[key] = {
+            'maintainers': ['uolip'],
+            'maintainers_keys': [key],
+        }
+
+        key = 'lib/ansible/modules/baz'
+        expected[key] = {
+            'maintainers': ['Loulou', 'ZaZa'],
+            'maintainers_keys': [key],
+        }
+
+        key = 'lib/ansible/modules/baz/test'
+        expected[key] = {
+            'maintainers': ['Loulou', 'jim', 'ZaZa'],
+            'maintainers_keys': ['lib/ansible/modules/baz', key],
+        }
+
+        key = 'lib/ansible/modules/baz/test/code.py'
+        expected[key] = {
+            'maintainers': ['Loulou', 'bob', 'jim', 'ZaZa'],
+            'maintainers_keys': ['lib/ansible/modules/baz', 'lib/ansible/modules/baz/test', key],
+        }
+
+        indexer = run(textwrap.dedent(self.BOTMETA), filepaths)
+
+        self.assertEqual(len(indexer.modules), len(filepaths))  # ensure only fake data are loaded
+
+        for k in expected:
+            # BOTMETA and modules have identical maintainers since there is no authors
+            # defined in source code
+            self.assertEqual(sorted(indexer.botmeta['files'][k]['maintainers_keys']),sorted(expected[k]['maintainers_keys']))
+            self.assertEqual(sorted(indexer.botmeta['files'][k]['maintainers']), sorted(expected[k]['maintainers']))
+
+        for k in filepaths:
+            self.assertEqual(sorted(indexer.modules[k]['maintainers_keys']),sorted(expected[k]['maintainers_keys']))
+            self.assertEqual(sorted(indexer.modules[k]['maintainers']), sorted(expected[k]['maintainers']))
+
