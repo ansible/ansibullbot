f9edad233dc562cbf44b19f8535470fad3feca77
James Tanner <tanner.jc@gmail.com>
2017-10-17 19:39:57 -0400

    Use the api association data to determine previous contributions

--- ansibullbot/triagers/plugins/contributors.py
+++ ansibullbot/triagers/plugins/contributors.py
@@ -1,48 +1,29 @@
 #!/usr/bin/env python
 
+import logging
+
 
 def get_contributor_facts(issuewrapper, meta, module_indexer, file_indexer, core_team=None, bot_names=None):
+
+    # https://github.com/blog/2397-making-it-easier-to-grow-communities-on-github
+    # NONE - new contributor
+    # MEMBER - member of a team
+    # CONTRIBUTOR - made previous commits
+
     cfacts = {
         'new_contributor': False
     }
 
     iw = issuewrapper
 
+    # ignore issues
     if iw.is_issue():
         return cfacts
 
-    # no bots
-    if bot_names and iw.submitter in bot_names:
-        return cfacts
+    association = iw.pull_raw.get('author_association')
+    logging.info('{} {} association: {}'.format(iw.html_url, iw.submitter, association))
 
-    # ignore ansible team
-    if core_team and iw.submitter in core_team:
-        return cfacts
-
-    # check commit log for user's email address(es)
-    emails = sorted(set(iw.committer_emails))
-    commits = file_indexer.commits_by_email(emails)
-    if commits:
-        return cfacts
-
-    # check sqlite
-    emails = module_indexer.get_emails_by_login(iw.submitter)
-    if emails:
-        return cfacts
-
-    # module docstrings
-    if iw.submitter in module_indexer.all_authors:
-        return cfacts
-
-    # botmeta
-    if iw.submitter in module_indexer.all_maintainers:
-        return cfacts
-
-    # blame data
-    ec = {v: k for k, v in module_indexer.emails_cache.iteritems()}
-    if iw.submitter in ec:
-        return cfacts
-
-    cfacts['new_contributor'] = True
+    if association is 'NONE':
+        cfacts['new_contributor'] = True
 
     return cfacts
