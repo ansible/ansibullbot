8cbf8bf9865b47cc7890aac3a0c60794f3a71142
James Tanner <tanner.jc@gmail.com>
2017-11-15 15:19:06 -0500

    fix shipit tests

--- tests/unit/triagers/plugins/test_shipit.py
+++ tests/unit/triagers/plugins/test_shipit.py
@@ -57,6 +57,11 @@ class MockFile(object):
         self.filename = name
 
 
+class MockRepo(object):
+    def __init__(self, repo_path):
+        self.repo_path = repo_path
+
+
 class TestShipitFacts(unittest.TestCase):
 
     def setUp(self):
@@ -161,7 +166,6 @@ class TestOwnerPR(unittest.TestCase):
             'is_needs_rebase': False,
         }
 
-    #@unittest.skip('disabled')
     def test_owner_pr_submitter_is_maintainer_one_module_utils_file_updated(self):
         """
         Submitter is a maintainer: ensure owner_pr is set (only one file below module_utils updated)
@@ -211,7 +215,6 @@ class TestOwnerPR(unittest.TestCase):
         self.assertEqual(iw.submitter, 'ElsA')
         self.assertTrue(facts['owner_pr'])
 
-    @unittest.skip('disabled')
     def test_owner_pr_submitter_is_maintainer_one_modules_file_updated(self):
         """
         Submitter is a maintainer: ensure owner_pr is set (only one file below modules updated)
@@ -232,20 +235,33 @@ class TestOwnerPR(unittest.TestCase):
         self.assertEqual(len(module_indexer.modules), 2)  # ensure only fake data are loaded
         self.assertEqual(sorted(module_indexer.botmeta['files']['lib/ansible/modules/foo/bar.py']['maintainers']), ['ElsA', 'mscherer'])
 
+        CM = ComponentMatcherMock()
+        CM.expected_results = [
+            {
+                'repo_filename': 'lib/ansible/modules/foo/bar.py',
+                'labels': [],
+                'support': None,
+                'maintainers': ['ElsA', 'mscherer'],
+                'notify': ['ElsA', 'mscherer'],
+                'ignore': [],
+            }
+        ]
+
         meta = self.meta.copy()
 
         datafile = 'tests/fixtures/shipit/0_issue.yml'
         statusfile = 'tests/fixtures/shipit/0_prstatus.json'
         with get_issue(datafile, statusfile) as iw:
             iw.pr_files = [MockFile('lib/ansible/modules/foo/bar.py')]
+            iw.file_indexer = FileIndexerMock()
+            iw.file_indexer.files.append('lib/ansible/modules/foo/bar.py')
 
-            meta.update(get_component_match_facts(iw, {}, FileIndexerMock(), module_indexer, []))
+            meta.update(get_component_match_facts(iw, {}, CM, iw.file_indexer, module_indexer, []))
             facts = get_shipit_facts(iw, meta, module_indexer, core_team=['bcoca'], botnames=['ansibot'])
 
         self.assertEqual(iw.submitter, 'mscherer')
         self.assertTrue(facts['owner_pr'])
 
-    @unittest.skip('disabled')
     def test_owner_pr_submitter_is_maintainer_new_module(self):
         """
         Submitter is a maintainer: pull request adds a new module: ensure owner_pr is False
@@ -267,20 +283,34 @@ class TestOwnerPR(unittest.TestCase):
         # Ensure that BOTMETA.yml updates doesn't interfere
         self.assertEqual(sorted(module_indexer.botmeta['files']['lib/ansible/modules/foo/bar.py']['maintainers']), ['ElsA', 'mscherer'])
 
+        CM = ComponentMatcherMock()
+        CM.expected_results = [
+            {
+                'repo_filename': 'lib/ansible/modules/foo/bar.py',
+                'labels': [],
+                'support': None,
+                'maintainers': ['ElsA', 'mscherer'],
+                'notify': ['ElsA', 'mscherer'],
+                'ignore': [],
+            }
+        ]
+
         meta = self.meta.copy()
 
         datafile = 'tests/fixtures/shipit/0_issue.yml'
         statusfile = 'tests/fixtures/shipit/0_prstatus.json'
         with get_issue(datafile, statusfile) as iw:
             iw.pr_files = [MockFile('lib/ansible/modules/foo/bar.py')]
+            iw.file_indexer = FileIndexerMock()
+            #iw.file_indexer.files.append('lib/ansible/modules/foo/bar.py')
 
-            meta.update(get_component_match_facts(iw, {}, FileIndexerMock(), module_indexer, []))
+            meta.update(get_component_match_facts(iw, {}, CM, iw.file_indexer, module_indexer, []))
             facts = get_shipit_facts(iw, meta, module_indexer, core_team=['bcoca'], botnames=['ansibot'])
 
         self.assertEqual(iw.submitter, 'mscherer')
         self.assertFalse(facts['owner_pr'])
 
-    @unittest.skip('disabled')
+    #@unittest.skip('disabled')
     def test_owner_pr_submitter_is_not_maintainer_of_all_updated_files(self):
         """
         PR updates 2 files below module_utils, submitter is a maintainer from only one: ensure owner_pr isn't set
@@ -303,6 +333,26 @@ class TestOwnerPR(unittest.TestCase):
         self.assertEqual(sorted(module_indexer.botmeta['files']['lib/ansible/module_utils/foo/bar.py']['maintainers']), ['ElsA', 'Oliver'])
         self.assertEqual(sorted(module_indexer.botmeta['files']['lib/ansible/module_utils/baz/bar.py']['maintainers']), ['TiTi', 'ZaZa'])
 
+        CM = ComponentMatcherMock()
+        CM.expected_results = [
+            {
+                'repo_filename': 'lib/ansible/module_utils/foo/bar.py',
+                'labels': [],
+                'support': None,
+                'maintainers': ['ElsA', 'Oliver'],
+                'notify': ['ElsA', 'Oliver'],
+                'ignore': [],
+            },
+            {
+                'repo_filename': 'lib/ansible/modules/baz/bar.py',
+                'labels': [],
+                'support': None,
+                'maintainers': ['TiTi', 'ZaZa'],
+                'notify': ['TiTi', 'ZaZa'],
+                'ignore': [],
+            }
+        ]
+
         issue = IssueMock('/dev/null')
         issue.user.login = 'ElsA'
         issue.html_url = 'https://github.com/ansible/ansible/pull/123'
@@ -311,15 +361,16 @@ class TestOwnerPR(unittest.TestCase):
             MockFile('lib/ansible/module_utils/foo/bar.py'),
             MockFile('lib/ansible/module_utils/baz/bar.py')
         ]
+        iw.file_indexer = FileIndexerMock()
+        iw.repo = MockRepo(repo_path='ansible/ansible')
 
         meta = self.meta.copy()
-        meta.update(get_component_match_facts(iw, {}, FileIndexerMock(), module_indexer, []))
+        meta.update(get_component_match_facts(iw, {}, CM, FileIndexerMock(), module_indexer, []))
         facts = get_shipit_facts(iw, meta, module_indexer, core_team=['bcoca', 'mscherer'], botnames=['ansibot'])
 
         self.assertEqual(iw.submitter, 'ElsA')
         self.assertFalse(facts['owner_pr'])
 
-    @unittest.skip('disabled')
     def test_owner_pr_module_utils_and_modules_updated_submitter_maintainer_1(self):
         """
         PR updates 2 files (one below modules, the other below module_utils),
@@ -345,6 +396,26 @@ class TestOwnerPR(unittest.TestCase):
         self.assertEqual(sorted(module_indexer.botmeta['files']['lib/ansible/modules/foo/bar.py']['maintainers']), ['ElsA', 'mscherer'])
         self.assertEqual(sorted(module_indexer.botmeta['files']['lib/ansible/module_utils/baz/bar.py']['maintainers']), ['TiTi', 'ZaZa'])
 
+        CM = ComponentMatcherMock()
+        CM.expected_results = [
+            {
+                'repo_filename': 'lib/ansible/module_utils/foo/bar.py',
+                'labels': [],
+                'support': None,
+                'maintainers': ['ElsA', 'mscherer'],
+                'notify': ['ElsA', 'mscherer'],
+                'ignore': [],
+            },
+            {
+                'repo_filename': 'lib/ansible/modules/baz/bar.py',
+                'labels': [],
+                'support': None,
+                'maintainers': ['TiTi', 'ZaZa'],
+                'notify': ['TiTi', 'ZaZa'],
+                'ignore': [],
+            }
+        ]
+
         meta = self.meta.copy()
 
         datafile = 'tests/fixtures/shipit/0_issue.yml'
@@ -354,13 +425,16 @@ class TestOwnerPR(unittest.TestCase):
                 MockFile('lib/ansible/modules/foo/bar.py'),
                 MockFile('lib/ansible/module_utils/baz/bar.py')
             ]
-            meta.update(get_component_match_facts(iw, {}, FileIndexerMock(), module_indexer, []))
+            iw.file_indexer = FileIndexerMock()
+            #iw.file_indexer.files.append('lib/ansible/modules/foo/bar.py'),
+            #iw.file_indexer.files.append('lib/ansible/module_utils/baz/bar.py')
+
+            meta.update(get_component_match_facts(iw, {}, CM, iw.file_indexer, module_indexer, []))
             facts = get_shipit_facts(iw, meta, module_indexer, core_team=['bcoca', 'mscherer'], botnames=['ansibot'])
 
         self.assertEqual(iw.submitter, 'mscherer')
         self.assertFalse(facts['owner_pr'])
 
-    @unittest.skip('disabled')
     def test_owner_pr_module_utils_and_modules_updated_submitter_maintainer_2(self):
         """
         PR updates 2 files (one below modules, the other below module_utils),
@@ -386,6 +460,26 @@ class TestOwnerPR(unittest.TestCase):
         self.assertEqual(sorted(module_indexer.botmeta['files']['lib/ansible/modules/foo/bar.py']['maintainers']), ['ElsA', 'ZaZa'])
         self.assertEqual(sorted(module_indexer.botmeta['files']['lib/ansible/module_utils/baz/bar.py']['maintainers']), ['TiTi', 'mscherer'])
 
+        CM = ComponentMatcherMock()
+        CM.expected_results = [
+            {
+                'repo_filename': 'lib/ansible/module_utils/foo/bar.py',
+                'labels': [],
+                'support': None,
+                'maintainers': ['ElsA', 'mscherer'],
+                'notify': ['ElsA', 'mscherer'],
+                'ignore': [],
+            },
+            {
+                'repo_filename': 'lib/ansible/modules/baz/bar.py',
+                'labels': [],
+                'support': None,
+                'maintainers': ['TiTi', 'ZaZa'],
+                'notify': ['TiTi', 'ZaZa'],
+                'ignore': [],
+            }
+        ]
+
         meta = self.meta.copy()
 
         datafile = 'tests/fixtures/shipit/0_issue.yml'
@@ -395,7 +489,8 @@ class TestOwnerPR(unittest.TestCase):
                 MockFile('lib/ansible/modules/foo/bar.py'),
                 MockFile('lib/ansible/module_utils/baz/bar.py')
             ]
-            meta.update(get_component_match_facts(iw, {}, FileIndexerMock(), module_indexer, []))
+            iw.file_indexer = FileIndexerMock()
+            meta.update(get_component_match_facts(iw, {}, CM, iw.file_indexer, module_indexer, []))
             facts = get_shipit_facts(iw, meta, module_indexer, core_team=['bcoca'], botnames=['ansibot'])
 
         self.assertEqual(iw.submitter, 'mscherer')
