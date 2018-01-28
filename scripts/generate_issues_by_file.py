6183d0a00c039d5b6624cb4532093ca69ea91ef2
James Tanner <tanner.jc@gmail.com>
2018-01-28 06:24:05 -0500

    Add script to emit data for web

---
+++ scripts/generate_issues_by_file.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+import json
+import os
+import sys
+
+from ansibullbot.utils.receiver_client import get_receiver_metadata
+from ansibullbot.utils.receiver_client import get_receiver_summaries
+
+
+def main():
+
+    # define where to dump the resulting files
+    if len(sys.argv) > 1:
+        destdir = sys.argv[1]
+    else:
+        destdir = '/tmp'
+
+    if not os.path.isdir(destdir):
+        os.makedirs(destdir)
+
+    ISSUES = {}
+    BYFILE = {}
+    BYISSUE = {}
+
+    summaries = get_receiver_summaries('ansible', 'ansible', state='open')
+    for summary in summaries:
+        number = summary['github_number']
+        this_meta = get_receiver_metadata('ansible', 'ansible', number=number)
+        this_meta = this_meta[0]
+        url = this_meta['html_url']
+        ISSUES[url] = this_meta
+        BYISSUE[url] = []
+
+        for component in this_meta.get('component_matches', []):
+            # we seem to have some variation in the keys ...
+            filename = None
+            try:
+                filename = component['repo_filename']
+            except KeyError:
+                filename = component['filename']
+
+            if not filename:
+                continue
+
+            BYISSUE[url].append(filename)
+
+            if filename not in BYFILE:
+                BYFILE[filename] = []
+            BYFILE[filename].append(url)
+
+    destfile = os.path.join(destdir, 'byissue.json')
+    with open(destfile, 'w') as f:
+        f.write(json.dumps(BYISSUE, indent=2, sort_keys=True))
+
+    destfile = os.path.join(destdir, 'byfile.json')
+    with open(destfile, 'w') as f:
+        f.write(json.dumps(BYFILE, indent=2, sort_keys=True))
+
+    tuples = BYFILE.items()
+    for idx, x in enumerate(tuples):
+        x = [x[0]] + x[1]
+        tuples[idx] = x
+    tuples.sort(key=len)
+    tuples.reverse()
+
+    destfile = os.path.join(destdir, 'byfile_sorted.txt')
+    with open(destfile, 'w') as f:
+        for tup in tuples:
+            f.write('{}\n'.format(tup[0]))
+            for issue in tup[1:]:
+                f.write('\t{}\n'.format(issue))
+
+
+if __name__ == "__main__":
+    main()
