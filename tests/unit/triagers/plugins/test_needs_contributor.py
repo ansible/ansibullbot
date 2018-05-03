ef2aa57d2b10037e1e618798af43978b248a4748
Martin Krizek <martin.krizek@gmail.com>
2018-05-03 15:50:44 +0200

    Fix needs_contributor workflow (#900)

---
+++ tests/unit/triagers/plugins/test_needs_contributor.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import unittest
+
+from ansibullbot.triagers.plugins.needs_contributor import get_needs_contributor_facts
+from tests.utils.helpers import get_issue
+
+
+class AnsibleTriageMock(object):
+    BOTNAMES = ['ansibot']
+
+
+class TestNeedsContributorFacts(unittest.TestCase):
+
+    def setUp(self):
+        self.meta = {}
+        self.statusfile = 'tests/fixtures/needs_contributor/0_prstatus.json'
+
+    def test_needs_contributor_command(self):
+        datafile = 'tests/fixtures/needs_contributor/0_issue.yml'
+        with get_issue(datafile, self.statusfile) as iw:
+            facts = get_needs_contributor_facts(AnsibleTriageMock(), iw, self.meta)
+            self.assertTrue(facts['is_needs_contributor'])
+
+    def test_not_needs_contributor_command(self):
+        datafile = 'tests/fixtures/needs_contributor/1_issue.yml'
+        with get_issue(datafile, self.statusfile) as iw:
+            facts = get_needs_contributor_facts(AnsibleTriageMock(), iw, self.meta)
+            self.assertFalse(facts['is_needs_contributor'])
+
+    def test_waiting_on_contributor_label(self):
+        datafile = 'tests/fixtures/needs_contributor/2_issue.yml'
+        with get_issue(datafile, self.statusfile) as iw:
+            facts = get_needs_contributor_facts(AnsibleTriageMock(), iw, self.meta)
+            self.assertTrue(facts['is_needs_contributor'])
