8f561582cb3f664f92ea4a855793ffcb294379e3
James Tanner <tanner.jc@gmail.com>
2018-05-08 21:29:15 -0400

    Extend git_tools to do more history analysis by email and files

--- ansibullbot/utils/git_tools.py
+++ ansibullbot/utils/git_tools.py
@@ -15,6 +15,8 @@ class GitRepoWrapper(object):
         self.checkoutdir = cachedir or '~/.ansibullbot/cache'
         self.checkoutdir = os.path.join(cachedir, 'ansible.git.checkout')
         self.checkoutdir = os.path.expanduser(self.checkoutdir)
+        self.commits_by_email = None
+        self.files_by_commit = {}
 
     @property
     def files(self):
@@ -33,13 +35,14 @@ class GitRepoWrapper(object):
         cmd = "git clone %s %s" \
             % (self.repo, self.checkoutdir)
         (rc, so, se) = run_command(cmd)
-        print str(so) + str(se)
+        print(str(so) + str(se))
 
     def update(self, force=False):
         '''Reload everything if there are new commits'''
         changed = self.manage_checkout()
         if changed or force:
             self.get_files(force=True)
+        self.commits_by_email = None
 
     def update_checkout(self):
         """rebase + pull + update the checkout"""
@@ -48,7 +51,7 @@ class GitRepoWrapper(object):
 
         cmd = "cd %s ; git pull --rebase" % self.checkoutdir
         (rc, so, se) = run_command(cmd)
-        print str(so) + str(se)
+        print(str(so) + str(se))
 
         # If rebase failed, recreate the checkout
         if rc != 0:
@@ -57,6 +60,8 @@ class GitRepoWrapper(object):
         else:
             if 'current branch devel is up to date.' not in so.lower():
                 changed = True
+
+        self.commits_by_email = None
 
         return changed
 
@@ -87,3 +92,59 @@ class GitRepoWrapper(object):
         with open(fpath, 'rb') as f:
             data = f.read()
         return data
+
+    def get_files_by_commit(self, commit):
+
+        if commit not in self.files_by_commit:
+            cmd = 'cd {}; git show --pretty="" --name-only {}'.format(self.checkoutdir, commit)
+            (rc, so, se) = run_command(cmd)
+            filenames = [x.strip() for x in so.split('\n') if x.strip()]
+            self.files_by_commit[commit] = filenames[:]
+        else:
+            filenames = self.files_by_commit[commit]
+
+        return filenames
+
+    def get_commits_by_email(self, email):
+        '''Map an email(s) to a total num of commits and total by file'''
+        if self.commits_by_email is None:
+            commits = {}
+            cmd = 'cd {}; git log --format="%h;%ae"'.format(self.checkoutdir)
+            (rc, so, se) = run_command(cmd)
+            lines = [x.strip() for x in so.split('\n') if x.strip()]
+            for line in lines:
+                parts = line.split(';')
+                this_hash = parts[0]
+                this_email = parts[1]
+                if this_email not in commits:
+                    commits[this_email] = set()
+                commits[this_email].add(this_hash)
+            self.commits_by_email = commits
+
+        if not isinstance(email, (set, list)):
+            emails = [email]
+        else:
+            emails = [x for x in email]
+
+        email_map = {}
+
+        for _email in emails:
+            if _email not in email_map:
+                email_map[_email] = {
+                    'commit_count': 0,
+                    'commit_count_byfile': {}
+                }
+
+            if _email in self.commits_by_email:
+
+                email_map[_email]['commit_count'] = \
+                    len(self.commits_by_email[_email])
+
+                for _commit in self.commits_by_email[_email]:
+                    filenames = self.get_files_by_commit(_commit)
+                    for fn in filenames:
+                        if fn not in email_map[_email]['commit_count_byfile']:
+                            email_map[_email]['commit_count_byfile'][fn] = 0
+                        email_map[_email]['commit_count_byfile'][fn] += 1
+
+        return email_map
