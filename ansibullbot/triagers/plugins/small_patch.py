f86770297633f3f19ee79e9578133b15e75cf258
Martin Krizek <martin.krizek@gmail.com>
2018-06-20 12:51:38 +0200

    Add small_patch label (#925)

---
+++ ansibullbot/triagers/plugins/small_patch.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import re
+
+
+FILE_MAX_CHANGED_LINES = 6
+SMALL_CHUNKS_MAX_COUNT = 2
+
+RE_CHUNK = r'@@ -\d+,\d+ \+\d+,\d+ @@'
+
+
+def get_small_patch_facts(iw):
+    sfacts = {
+        'is_small_patch': False
+    }
+
+    small_chunks_changed = 0
+
+    for commit in iw.get_commits():
+        for changed_file in commit.files:
+            if changed_file.filename.startswith('test/'):
+                continue
+
+            if not changed_file.raw_data['status'] == 'modified':
+                return sfacts
+
+            chunks_in_file_count = len(re.findall(RE_CHUNK, changed_file.raw_data['patch']))
+
+            if changed_file.changes > FILE_MAX_CHANGED_LINES:
+                return sfacts
+            elif changed_file.changes:
+                small_chunks_changed += chunks_in_file_count
+
+        if small_chunks_changed > SMALL_CHUNKS_MAX_COUNT:
+            return sfacts
+
+
+    if small_chunks_changed:
+        sfacts['is_small_patch'] = True
+
+    return sfacts
