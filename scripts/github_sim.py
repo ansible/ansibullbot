b2d3927ebb600c5f351a18e01817ddaa7f2eac8b
James Tanner <tanner.jc@gmail.com>
2018-10-14 23:03:53 -0400

    Start refactoring the simulator to be more comprehensive

--- scripts/github_sim.py
+++ scripts/github_sim.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import six
+import time
 
 from flask import Flask
 from flask import jsonify
@@ -11,12 +12,78 @@ app = Flask(__name__)
 
 ERROR_TIMER = 0
 
+TOKENS = {
+    'AAA': 'abot'
+}
+
+ISSUES = {
+    'github': {}
+}
+
+
+def get_issue(org, repo, number):
+
+    key = (org, repo, number)
+    if key in ISSUES:
+        return ISSUES[key]
+
+    url = 'http://localhost:5000'
+    url += '/'
+    url += 'repos'
+    url += '/'
+    url += org
+    url += '/'
+    url += repo
+    url += '/'
+    url += 'issues'
+    url += '/'
+    url += str(number)
+
+    h_url = 'http://localhost:5000'
+    h_url += '/'
+    h_url += org
+    h_url += '/'
+    h_url += repo
+    h_url += '/'
+    h_url += 'issues'
+    h_url += '/'
+    h_url += str(number)
+
+    e_url = 'http://localhost:5000'
+    e_url += '/'
+    e_url += org
+    e_url += '/'
+    e_url += repo
+    e_url += '/'
+    e_url += 'issues'
+    e_url += '/'
+    e_url += str(number)
+    e_url += '/'
+    e_url += 'events'
+
+    return {
+        'assignees': [],
+        'created_at': '2018-09-12T21:14:02Z',
+        'updated_at': '2018-09-12T21:24:05Z',
+        'url': url,
+        'events_url': e_url,
+        'html_url': h_url,
+        'number': int(number),
+        'labels': [],
+        'user': {
+            'login': 'foouser'
+        },
+        'title': 'this thing is broken',
+        'body': '',
+        'state': 'open'
+    }
+
 
 def error_time():
     global ERROR_TIMER
     print('ERROR_TIMER: %s' % ERROR_TIMER)
     ERROR_TIMER += 1
-    if ERROR_TIMER >= 10:
+    if ERROR_TIMER >= 100:
         ERROR_TIMER = 0
         return True
     else:
@@ -54,18 +121,19 @@ def root():
 
 @app.route('/rate_limit')
 def rate_limit():
+    reset = int(time.time()) + 10
     rl = {
         'resources': {
             'core': {
                 'limit': 5000,
                 'remaining': 5000,
-                'reset': 1536808348
+                'reset': reset
             }
         },
         'rate': {
             'limit': 5000,
             'remaining': 5000,
-            'reset': 1536808348
+            'reset': reset
         }
     }
     return jsonify(rl)
@@ -92,9 +160,11 @@ def orgs(path):
         return jsonify([])
 
 
-@app.route('/repos/<path:path>')
+@app.route('/repos/<path:path>', methods=['GET', 'POST'])
 def repos(path):
     # http://localhost/repos/ansible/ansible/labels
+    # http://localhost/repos/ansible/ansible/issues/1/comments
+
     path_parts = path.split('/')
     print(six.text_type((len(path_parts),path_parts)))
 
@@ -118,60 +188,23 @@ def repos(path):
         print('sending repo labels')
         return jsonify([])
 
+    elif path_parts[-1] == 'comments':
+        if request.method == 'POST':
+            print('adding comment(s)')
+            print(request.headers)
+            print(request.data)
+            '''
+            {"body": "blah blah blah"}
+            '''
+            import epdb; epdb.st()
+            return jsonify({})
+        else:
+            return jsonify([])
+
     elif len(path_parts) == 4 and path_parts[-2] == 'issues':
-
-        url = 'http://localhost:5000'
-        url += '/'
-        url += 'repos'
-        url += '/'
-        url += path_parts[0]
-        url += '/'
-        url += path_parts[1]
-        url += '/'
-        url += 'issues'
-        url += '/'
-        url += path_parts[-1]
-
-        h_url = 'http://localhost:5000'
-        h_url += '/'
-        h_url += path_parts[0]
-        h_url += '/'
-        h_url += path_parts[1]
-        h_url += '/'
-        h_url += 'issues'
-        h_url += '/'
-        h_url += path_parts[-1]
-
-        e_url = 'http://localhost:5000'
-        e_url += '/'
-        e_url += path_parts[0]
-        e_url += '/'
-        e_url += path_parts[1]
-        e_url += '/'
-        e_url += 'issues'
-        e_url += '/'
-        e_url += path_parts[-1]
-        e_url += '/'
-        e_url += 'events'
-        #import epdb; epdb.st()
-
         print('sending issue')
-        return jsonify({
-            'assignees': [],
-            'created_at': '2018-09-12T21:14:02Z',
-            'updated_at': '2018-09-12T21:24:05Z',
-            'url': url,
-            'events_url': e_url,
-            'html_url': h_url,
-            'number': int(path_parts[-1]),
-            'labels': [],
-            'user': {
-                'login': 'foouser'
-            },
-            'title': 'this thing is broken',
-            'body': '',
-            'state': 'open'
-        })
+        return jsonify(get_issue(path_parts[0], path_parts[1], path_parts[-1]))
+
     elif len(path_parts) == 5 and path_parts[-1] == 'comments':
         print('sending comments')
         return jsonify([])
@@ -184,6 +217,7 @@ def repos(path):
     elif len(path_parts) == 2:
         return jsonify({})
 
+    print('unhandled path ...')
     print(six.text_type((len(path_parts),path_parts)))
 
 
