1ab5241763f4754dc97cfcbd514825239c5db181
James Tanner <tanner.jc@gmail.com>
2019-07-22 10:26:40 -0400

    Do not set has_pr|issue if it was unlabeled at any point

--- ansibullbot/triagers/plugins/cross_references.py
+++ ansibullbot/triagers/plugins/cross_references.py
@@ -7,7 +7,9 @@ def get_cross_reference_facts(issuewrapper, meta):
 
     crfacts = {
         u'has_pr': False,
-        u'has_issue': False
+        u'has_issue': False,
+        u'needs_has_pr': False,
+        u'needs_has_issue': False,
     }
 
     cross_refs = [x for x in iw.events if x['event'] == 'cross-referenced']
@@ -23,5 +25,13 @@ def get_cross_reference_facts(issuewrapper, meta):
         crfacts[u'has_pr'] = True
     elif iw.is_pullrequest() and issues:
         crfacts[u'has_issue'] = True
+
+    if crfacts[u'has_pr']:
+        if not iw.history.was_unlabeled(u'has_pr'):
+            crfacts[u'needs_has_pr'] = True
+
+    if crfacts[u'has_issue']:
+        if not iw.history.was_unlabeled(u'has_issue'):
+            crfacts[u'needs_has_issue'] = True
 
     return crfacts
