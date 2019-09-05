283be98c5db790ad93331dae143d24089919eed7
James Tanner <tanner.jc@gmail.com>
2019-09-05 15:10:17 -0400

    move file

--- tests/unit/utils/test_extractors_pr_number.py
+++ tests/unit/utils/test_extractors_pr_number.py
@@ -13,11 +13,6 @@ from ansibullbot.utils.extractors import extract_pr_number_from_comment
     ('resolved_by_pr https://github.com/ansible/ansible/issues/5136', 5136),
     ('resolved_by_pr: https://github.com/ansible/ansible/issues/5136', 5136)
 ])
-#@pytest.mark.parametrize('resolved_by_pr: 5136', [5136])
-#@pytest.mark.parametrize('resolved_by_pr #5136', [5136])
-#@pytest.mark.parametrize('resolved_by_pr: #5136', [5136])
-#@pytest.mark.parametrize('resolved_by_pr https://github.com/ansible/ansible/issues/5136', [5136])
-#@pytest.mark.parametrize('resolved_by_pr: https://github.com/ansible/ansible/issues/5136', [5136])
 def test_extract_pr_number_from_comment(test_input, expected):
     assert extract_pr_number_from_comment(test_input) == expected
     
