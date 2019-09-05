0007fd5a3453aac5fca3fe85267fe17799fe4f7a
James Tanner <tanner.jc@gmail.com>
2019-09-05 15:20:37 -0400

    add negative testing

--- tests/unit/utils/test_extractors_pr_number.py
+++ tests/unit/utils/test_extractors_pr_number.py
@@ -11,7 +11,15 @@ from ansibullbot.utils.extractors import extract_pr_number_from_comment
     ('resolved_by_pr #5136', 5136),
     ('resolved_by_pr: #5136', 5136),
     ('resolved_by_pr https://github.com/ansible/ansible/issues/5136', 5136),
-    ('resolved_by_pr: https://github.com/ansible/ansible/issues/5136', 5136)
+    ('resolved_by_pr: https://github.com/ansible/ansible/issues/5136', 5136),
+    ('resolved_by_pr:', None),
+    ('resolved_by_pr', None),
+    ('resolved_by_pr XXXX', None),
+    ('resolved_by_pr: XXXX', None),
+    ('resolved_by_pr https://github.com/ansible/ansible/issues/', None),
+    ('resolved_by_pr: https://github.com/ansible/ansible/issues/', None),
+    ('resolved_by_pr https://github.com/ansible/ansible/issues/X', None),
+    ('resolved_by_pr: https://github.com/ansible/ansible/issues/X', None),
 ])
 def test_extract_pr_number_from_comment(test_input, expected):
     assert extract_pr_number_from_comment(test_input) == expected
