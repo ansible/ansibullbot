ab9b37da9b6da39cf6a3b4992cf394b14c1daec7
Martin Krizek <martin.krizek@gmail.com>
2020-07-01 11:41:40 +0200

    Fix

--- ansibullbot/triagers/plugins/needs_revision.py
+++ ansibullbot/triagers/plugins/needs_revision.py
@@ -91,8 +91,8 @@ def get_needs_revision_facts(triager, issuewrapper, meta, shippable):
     if ci_states:
         has_shippable = True
         ci_stale = shippable.is_stale(ci_states)
+        ci_state = shippable.get_state(ci_states)
 
-    ci_state = shippable.get_state(ci_states)
     logging.info(u'ci_state == %s' % ci_state)
 
     # clean/unstable/dirty/unknown
