755799a4c7c77cd769965ec53d34f8c334cb9719
Martin Krizek <martin.krizek@gmail.com>
2020-10-12 12:01:47 +0200

    Do not require version for docs issues (#1447)

--- ansibullbot/triagers/plugins/needs_info.py
+++ ansibullbot/triagers/plugins/needs_info.py
@@ -62,7 +62,7 @@ def needs_info_template_facts(iw, meta):
     if not iw.template_data:
         nifacts[u'template_missing'] = True
 
-    itype = iw.template_data.get(u'issue type')
+    itype = iw.template_data.get(u'issue type', '')
     missing = []
 
     # theoretically we only need to know the issue type for a PR
@@ -72,7 +72,7 @@ def needs_info_template_facts(iw, meta):
         expected = [u'issue type', u'ansible version', u'component name']
 
     for exp in expected:
-        if itype == u'feature idea' and exp == u'ansible version':
+        if itype.lower() in (u'feature idea', u'documentation report') and exp == u'ansible version':
             continue
         if exp not in iw.template_data or not iw.template_data[exp]:
             missing.append(exp)
