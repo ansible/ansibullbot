9a193b3d2f023341a5bdd8cf4973dd57f3ed3a8c
Martin Krizek <martin.krizek@gmail.com>
2020-11-03 16:12:54 +0100

    Add !bot_broken command (#1459)

--- ansibullbot/wrappers/historywrapper.py
+++ ansibullbot/wrappers/historywrapper.py
@@ -319,8 +319,6 @@ class HistoryWrapper(object):
                     if event[u'body'].startswith(u'_From @'):
                         continue
                     l_body = event[u'body'].split()
-                    if y != u'bot_broken' and u'bot_broken' in l_body:
-                        continue
                     if y in l_body and not u'!' + y in l_body:
                         if timestamps:
                             commands.append((event[u'created_at'], y))
