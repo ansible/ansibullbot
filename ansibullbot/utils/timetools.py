71ba4cf8c224143d66ff7c6acd5e7c5ae62461bf
Martin Krizek <martin.krizek@gmail.com>
2020-11-13 12:13:06 +0100

    Account for "unconverted data remains"

--- ansibullbot/utils/timetools.py
+++ ansibullbot/utils/timetools.py
@@ -1,10 +1,11 @@
 import datetime
 import logging
 
+from ansibullbot._text_compat import to_text
+
 
 def strip_time_safely(tstring):
     """Try various formats to strip the time from a string"""
-    res = None
     tsformats = (
         u'%Y-%m-%dT%H:%M:%SZ',
         u'%Y-%m-%dT%H:%M:%S.%f',
@@ -13,13 +14,19 @@ def strip_time_safely(tstring):
         u'%a %b %d %H:%M:%S %Y',
         u'%Y-%m-%d',
     )
+
     for tsformat in tsformats:
         try:
-            res = datetime.datetime.strptime(tstring, tsformat)
-            break
+            return datetime.datetime.strptime(tstring, tsformat)
         except ValueError as e:
-            logging.debug(e)
-    if res is None:
-        logging.error(u'{} could not be stripped'.format(tstring))
-        raise Exception(u'{} could not be stripped'.format(tstring))
-    return res
+            text_e = to_text(e)
+            if u'unconverted data remains' in text_e and tstring.endswith('Z'):
+                # '2020-11-10T07:39:58.6833333Z'
+                new_tstring = tstring[:-len(text_e.split(':')[-1].strip())]+'Z'
+                try:
+                    return datetime.datetime.strptime(new_tstring, '%Y-%m-%dT%H:%M:%S.%fZ')
+                except ValueError:
+                    pass
+
+    logging.error(u'{} could not be stripped'.format(tstring))
+    raise Exception(u'{} could not be stripped'.format(tstring))
