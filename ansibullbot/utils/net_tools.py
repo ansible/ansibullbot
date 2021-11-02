e010ad2070d9bb449ab7b6c05a60ff8e1882605c
Martin Krizek <martin.krizek@gmail.com>
2021-11-02 13:53:00 +0100

    Use stdlib for retries (#1591)

--- ansibullbot/utils/net_tools.py
+++ ansibullbot/utils/net_tools.py
@@ -1,7 +1,7 @@
 import logging
+import time
 
 import requests
-from tenacity import retry, stop_after_attempt, wait_fixed, RetryError, TryAgain
 
 
 # FIXME should we only retry 5xx?
@@ -17,8 +17,7 @@ _DONT_RETRY_STATUSES = [
 
 def fetch(url, verb='get', **kwargs):
     """return response or None in case of failure, try twice"""
-    @retry(stop=stop_after_attempt(2), wait=wait_fixed(2))
-    def _inner_fetch(verb='get'):
+    for i in range(2):
         logging.info('%s %s' % (verb, url))
         http_method = getattr(requests, verb)
         resp = http_method(url, **kwargs)
@@ -27,12 +26,7 @@ def fetch(url, verb='get', **kwargs):
 
         if resp.status_code not in _DONT_RETRY_STATUSES:
             logging.error('status code: %s' % resp.status_code)
-            raise TryAgain
+            time.sleep(2)
+            continue
 
         return resp
-
-    try:
-        logging.debug('%s' % url)
-        return _inner_fetch(verb=verb)
-    except RetryError as e:
-        logging.error(e)
