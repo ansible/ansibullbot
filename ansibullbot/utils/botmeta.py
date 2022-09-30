d30e468b6cfd7441b529c75ecb73fc1283450293
Martin Krizek <martin.krizek@gmail.com>
2022-09-30 12:11:20 +0200

    Remove unused utils/botmeta.py

--- ansibullbot/utils/botmeta.py
+++
@@ -1,9 +0,0 @@
-def botmeta_list(inlist):
-    '''use the bot's expansion of space separated lists feature'''
-    if not isinstance(inlist, list):
-        return inlist
-    # can't join words with spaces in them
-    if [x for x in inlist if ' ' in x]:
-        return inlist
-    else:
-        return ' '.join(sorted([x.strip() for x in inlist if x.strip()]))
